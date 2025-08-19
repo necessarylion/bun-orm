55dcca7c760d9c4b52562ccd28f3137eb2575ae6
necessarylion <aj.zinkyaw@gmail.com>
2025-08-19 19:42:29 +0700

    apply linter

--- test/having.test.ts
+++ test/having.test.ts
@@ -12,30 +12,20 @@ describe('Having', () => {
   })
 
   it('having equal', async () => {
-    const users = await db.table('users')
-      .groupBy('users.id')
-      .having('email', 'bob@example.com')
-      .get()
+    const users = await db.table('users').groupBy('users.id').having('email', 'bob@example.com').get()
 
     expect(users.length).toBe(1)
     expect(users[0].email).toBe('bob@example.com')
   })
 
   it('having greater than', async () => {
-    const users = await db.table('users')
-      .groupBy('users.id')
-      .having('age', '>', 25)
-      .get()
+    const users = await db.table('users').groupBy('users.id').having('age', '>', 25).get()
 
     expect(users.length).toBe(3)
   })
 
   it('where and having greater than', async () => {
-    const users = await db.table('users')
-      .where('active', true)
-      .groupBy('users.id')
-      .having('age', '>', 25)
-      .get()
+    const users = await db.table('users').where('active', true).groupBy('users.id').having('age', '>', 25).get()
 
     expect(users.length).toBe(2)
   })
@@ -47,7 +37,8 @@ describe('Having', () => {
       age: 25,
       active: true,
     })
-    const users = await db.table('users')
+    const users = await db
+      .table('users')
       .select('age')
       .select('count(*) as count')
       .where('active', true)
